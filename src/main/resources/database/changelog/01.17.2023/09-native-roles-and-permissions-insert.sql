5693ff861b2c765ff6bdb34914e2bcfbf60d8cca
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-01-18 00:28:05 +0100

    create db migration (insert app native roles and permissions)

--- src/main/resources/database/changelog/01.17.2023/09-native-roles-and-permissions-insert.sql
+++ src/main/resources/database/changelog/01.17.2023/09-native-roles-and-permissions-insert.sql
@@ -1,7 +1,10 @@
-INSERT INTO role (name) VALUES 
-("ROLE_USER"), ("ROLE_MANAGER"), ("ROLE_ADMIN"), ("ROLE_SYSADMIN")
+--liquibase formatted sql
 
-INSERT INTO permission (name) VALUES 
+--changeset bnedza:9
+INSERT INTO role (name) VALUES
+("ROLE_USER"), ("ROLE_MANAGER"), ("ROLE_ADMIN"), ("ROLE_SYSADMIN");
+
+INSERT INTO permission (name) VALUES
 ("READ:USER"),
 ("READ:CUSTOMER"),
 ("CREATE:USER"),
@@ -15,4 +18,4 @@ insert into role_permission (role_id, permission_id) values
 (1,1), (1,2), #user
 (2,1), (2,2), (2,5), (2,6), #manager
 (3,1), (3,2), (3,3), (3,4), (3,5), (3,6), #admin
-(4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7), (4,8) #sysadmin
+(4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7), (4,8); #sysadmin
