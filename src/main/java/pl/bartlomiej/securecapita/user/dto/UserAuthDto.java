9664f3726f09f1bda81d60b6ff0af6ea0ac29243
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-01-31 14:10:34 +0100

    typo correction

--- src/main/java/pl/bartlomiej/securecapita/user/dto/UserAuthDto.java
+++ src/main/java/pl/bartlomiej/securecapita/user/dto/UserAuthDto.java
@@ -3,5 +3,5 @@ package pl.bartlomiej.securecapita.user.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserAuthDto(@NotBlank(message = "Email cannot be empty.") String email,
-                          @NotBlank(message = "Password field cannor be empty.") String password) {
+                          @NotBlank(message = "Password field cannot be empty.") String password) {
 }
